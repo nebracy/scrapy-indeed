105de5db3864641cf03326f01b1c4f990bc6ad3c
Nicole B <contact@nicolebracy.com>
2022-01-20 13:14:59 -0500

    Delete commented notes and modify import

--- scrapyindeed/items.py
+++ scrapyindeed/items.py
@@ -1,12 +1,6 @@
-# Define here the models for your scraped items
-#
-# See documentation in:
-# https://docs.scrapy.org/en/latest/topics/items.html
-
-import scrapy
+from scrapy import Item, Field
 
 
-class ScrapyindeedItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
+class ScrapyindeedItem(Item):
+    # name = Field()
     pass
